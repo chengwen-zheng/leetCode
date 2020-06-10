d9bcebe8737751da57fdad55da46ce7f0a473920
leon.zheng <leon.zheng@derbysoft.com>
2020-06-10 10:07:00 +0800

    update 011-longestConsecutive.js

--- daily-algorithm-exercises/011-longestConsecutive.js
+++ daily-algorithm-exercises/011-longestConsecutive.js
@@ -40,4 +40,31 @@ var longestConsecutive = function (nums) {
     return res.length;
 };
 
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2, 6, 8098, 5,89]));
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutivePro = function (nums) {
+    let num_set = new Set(nums);
+    for (const num of nums) {
+        num_set.add(num);
+    }
+
+    let maxNum = 0;
+    for (const num of num_set) {
+        if (!num_set.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (num_set.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+
+            maxNum = Math.max(maxNum, currentStreak);
+        }
+    }
+    return maxNum;
+}
+
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2, 6, 8098, 5, 89]));

bd06405419ae3126f8cba1f356f475ec24bfd639
leon.zheng <chengwenzheng1222@gmail.com>
2020-06-28 12:06:49 +0800

    feat:(add 018-numTrees.js)

---
+++ daily-algorithm-exercises/018-numTrees.js
@@ -0,0 +1,39 @@
+/**
+ 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+
+ 示例:
+
+ 输入: 3
+ 输出: 5
+ 解释:
+ 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+
+ 1         3     3      2      1
+ \       /     /      / \      \
+ 3     2     1      1   3      2
+ /     /       \                 \
+ 2     1         2                 3
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/unique-binary-search-trees*/
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numTrees = function (n) {
+    let dp = Array.from({length: n + 1}, () => 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i < n + 1; i++) {
+        for (let j = 1; j < i + 1; j++) {
+            dp[i] += dp[j - 1] * dp[i - j]
+        }
+    }
+
+    return dp[n];
+};
+
+console.log(numTrees(3))

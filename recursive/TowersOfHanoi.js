102e88164c405fa2a588489bd37dd0a8db01a916
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-02-24 11:59:42 +0800

    feat: TowerOfHanoi

---
+++ recursive/TowersOfHanoi.js
@@ -0,0 +1,22 @@
+
+var moveCount = 0;
+
+function move(nDisk, from, to) {
+  console.log("第" + (++moveCount) +" 次移动 : " +" 把 "+ nDisk+" 号圆盘从 " + from +" ->移到->  " + to);
+}
+
+function hanoi(n, from, to, aux) {
+  if (n == 1) {
+    move(n, from, to);
+  } else {
+    hanoi(n - 1, from, aux, to);
+    move(n, from, to);
+    hanoi(n - 1, aux, to, from);
+  }
+}
+
+function main() {
+  hanoi(3, "A", "C", "B");
+}
+
+main();
